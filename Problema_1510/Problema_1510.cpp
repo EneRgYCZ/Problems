f390a0c5303ada5990cddc0f335b7241ec8bbe75
UltraGamingSetup <catalin.zaharia2013@gmail.com>
2020-11-24 10:39:28 +0200

    Main PC

--- Problema_1510/Problema_1510.cpp
+++ Problema_1510/Problema_1510.cpp
@@ -15,7 +15,7 @@ int cautare(int n, double X[], double v)
 
 int main()
 {
-    int n = 5, 
+    int n = 5;
     double X[101] = {4, 5, 0, 9, 10, 7, 15, 3, 8, 10}, v;
     cautare(n, X, v);
     for (int i = 0; i < n * 2; i++)
